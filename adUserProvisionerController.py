63fb7e0d4f2d6203911098bba4d106253cf1b12b
Sam  Chapman <sam.chapman@outlook.com>
2021-04-04 00:16:27 +0100

    update - think I broke the controller

--- adUserProvisionerController.py
+++ adUserProvisionerController.py
@@ -4,4 +4,10 @@ from userInterface.userCreationMainWindow import Ui_MainWindow
 
 #Controller class
 class adUserCreationCtrl:
-    
+    #PyCalc Controller class.
+    def __init__(self, model, view):
+        #Controller initializer.
+        self._evaluate = model
+        self._view = view
+        # Connect signals and slots
+        self._connectSignals()
