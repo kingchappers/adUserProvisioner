7bb389d3035ac557ab85e27f5e1422a6a86d95b5
Sam  Chapman <sam.chapman@outlook.com>
2021-04-04 00:36:47 +0100

    added controller class

---
+++ adUserProvisionerMain.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from userInterface.userCreationMainWindow import Ui_MainWindow
+from adUserProvisionerController import adUserCreationCtrl
+
+def main():
+    #Main function.
+    # Create an instance of QApplication
+    pycalc = QApplication(sys.argv)
+    # Show the calculator's GUI
+    view = PyCalcUi()
+    view.show()
+    # Create instances of the model and the controller
+    model = evaluateExpression
+    adUserCreationCtrl(model=model, view=view)
+    # Execute the calculator's main loop
+    sys.exit(pycalc.exec_())
--- adUserProvisionerModel.py
+++ adUserProvisionerModel.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 class adProvisionerMdl:
     def commandBuilder(self):
         x=1
