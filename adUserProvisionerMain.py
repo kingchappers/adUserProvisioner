7c1f58c7256965bc670baf119c9237f2ec4025b6
Sam  Chapman <sam.chapman@outlook.com>
2021-04-04 00:55:14 +0100

    addiions

--- adUserProvisionerMain.py
+++ adUserProvisionerMain.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QApplication
 
 from userInterface.userCreationMainWindow import Ui_MainWindow
 from adUserProvisionerController import adUserCreationCtrl
+from adUserProvisioner.userInterface.userCreationUI import Ui_userCreationWindow
 
 def main():
     #Main function.
