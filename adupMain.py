b9abbfef9057e869854cbf8c254365aa044dc68f
Sam  Chapman <sam.chapman@outlook.com>
2021-04-10 14:04:15 +0100

    adupUI now displays, displays as pycalc ui but displays so I'm happy

--- adupMain.py
+++ adupMain.py
@@ -4,16 +4,20 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from adupCtrl import adUserCreationCtrl
+from adupUI import adupUI
 
 def main():
     #Main function.
     # Create an instance of QApplication
-    pycalc = QApplication(sys.argv)
+    adup = QApplication(sys.argv)
     # Show the calculator's GUI
-    view = userCreationUI.Ui_userCreationUIWindow()
+    view = adupUI()
     view.show()
     # Create instances of the model and the controller
     #model = evaluateExpression
     adUserCreationCtrl(view=view)
     # Execute the calculator's main loop
-    sys.exit(pycalc.exec_())
+    sys.exit(adup.exec_())
+
+if __name__ == '__main__':
+    main()
