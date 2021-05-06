b3dcddb6631964c48efe3d2b0ed242a5a01e2639
Sam  Chapman <sam.chapman@outlook.com>
2021-05-06 18:05:38 +0100

    added bits to ctrl to help test

--- adup_ctrl.py
+++ adup_ctrl.py
@@ -11,4 +11,10 @@ class AdupCtrl:
         self._model = model
         # Connect signals and slots
         #self._connectSignals()
-        
+    
+    def populate_view_dropdowns(self):
+        """Populate the view's dropdown options"""
+        config_file_location = './config/config.json'
+        json_data = self._model.read_json_file(config_file_location)
+        print(json_data['company'][2]['department'][0]['roles'][0])
+        #print(json_data['company'].keys())
