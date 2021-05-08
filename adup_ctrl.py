5ce6b0c41453ef291784df67c317456b7c70933b
Sam  Chapman <sam.chapman@outlook.com>
2021-05-08 22:21:40 +0100

    updated get next combobox element function, it now gets the values for the department list and returns them

--- adup_ctrl.py
+++ adup_ctrl.py
@@ -12,13 +12,26 @@ class AdupCtrl:
         """Populate the view's dropdown options"""
         config_file_location = './config/config.json'
         json_data = self._model.read_json_file(config_file_location)
-
+        print(type(json_data.keys()))
         return json_data.keys()
 
-    def get_next_combo_element(self):
+    def get_next_combo_element(self, combobox_text, combo_name):
+        """Select the next values of the combobox based on the selection of another"""
         config_file_location = './config/config.json'
         json_data = self._model.read_json_file(config_file_location)
-        
-        print(json_data['Holdings'])
+        department_list = list()
 
-        print("butts")
+
+        for i in json_data[combobox_text]:
+            #print(i)
+            #print(i['deptName'])
+            #print(list(i.values())[0])
+            department_list.append(list(i.values())[0])
+            #print(type(list(i.values())[0]))
+            #return "poop"
+
+        #print(department_list)
+        return department_list
+        #print(json_data[combobox_text]['deptName'])
+        #print(combobox_text)
+        #print(json_data.items())
